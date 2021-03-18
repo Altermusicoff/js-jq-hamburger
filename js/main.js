446bce7b3ca7cb0e3e158517b409508b412954cd
Alessndro <alessa.dongu@gmail.com>
2021-03-18 17:08:39 +0100

    update

---
+++ js/main.js
@@ -0,0 +1,19 @@
+    //SOLUZIONE 1
+
+// $('.fa-bars').click(function(){
+//     $('.hamburger-menu').show();
+//     });
+// $('.fa-times').click(function(){
+//     $('.hamburger-menu').hide();
+//     });
+    
+    //SOLUZIONE 2
+// $('.fa-bars,.fa-times').click(function () {
+//     $('.hamburger-menu').toggle();
+//     });
+
+    //SOLUZIONE 2 ANIMAZIONE SE INSERISCO UNA STRINGA DENTRO TOGGLE
+$('.fa-bars,.fa-times').click(function () {
+    $('.hamburger-menu').toggle.delay(slow);
+});
+
